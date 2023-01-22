48b2b8b162e10197d632c749b494688bdc992753
Joao-Carmo <40190116@esmad.ipp.pt>
2023-01-22 23:46:55 +0000

    image uploading for attractions

--- controllers/attractions.controller.js
+++ controllers/attractions.controller.js
@@ -3,10 +3,13 @@ const db = require("../models/index.js");
 const bcrypt = require('bcrypt');
 const Attraction = db.attraction;
 const { Op } = require("sequelize");
+const cloudinary  = require('../config/cloudinary.config.js');
 
 exports.create = async (req, res) => {
     try {
       let attraction = await Attraction.findOne({ where: { name: req.body.name } });
+      const upload = await cloudinary.uploader.upload(req.file.path);
+
       if (attraction) {
         return res.status(400).json({ message: "That attraction already exists." });
       }
@@ -15,7 +18,7 @@ exports.create = async (req, res) => {
                 name: req.body.name,
                 address: req.body.address,
                 description: req.body.description,
-                image: req.body.image,
+                image: upload.url,
                 longitude: req.body.longitude,
                 latitude: req.body.latitude,
           });
